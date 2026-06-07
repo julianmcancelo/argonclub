9e3e54ac654e7466a07931861316bea83a7d656a
Julian Cancelo <jcancelo.dev@gmail.com>
2026-06-07 15:38:48 -0300

    build: Add Vercel build script and config to automatically compile Flutter

---
+++ vercel_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script de compilacion para Vercel (Instala Flutter temporalmente y compila)
+
+echo "--- Iniciando instalacion de Flutter en Vercel ---"
+git clone https://github.com/flutter/flutter.git -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
+
+echo "--- Habilitando Flutter Web ---"
+flutter config --enable-web
+
+echo "--- Instalando dependencias ---"
+flutter pub get
+
+echo "--- Compilando la aplicacion ---"
+flutter build web --release
+
+echo "--- Compilacion finalizada con exito ---"
